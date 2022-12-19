e02a85566c41b02d6a1bb1407a373139f8e005f3
Ryder-672 <91911144+Ryder-672@users.noreply.github.com>
2022-12-19 12:17:01 +0100

    06 07

--- Relacion02/src/Ejercicios/ej06.java
+++ Relacion02/src/Ejercicios/ej06.java
@@ -1,10 +1,44 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class ej06 {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("introduce un numero: ");
+		double a1 = teclado.nextDouble();
+		System.out.println("introduce otro numero: ");
+		double b1 = teclado.nextDouble();
+		System.out.println("introduce un ultimo numero: ");
+		double c1 = teclado.nextDouble();
+		
+		System.out.println("introduce un numero: ");
+		double a2 = teclado.nextDouble();
+		System.out.println("introduce otro numero: ");
+		double b2 = teclado.nextDouble();
+		System.out.println("introduce un ultimo numero: ");
+		double c2 = teclado.nextDouble();
+		
+		if ((a1/a2) != (b1/b2)) {
+			
+			if  ((a1/b1) == (-b2/a2)) {
+				System.out.println("son rectas perpendiculares ");	
+			}else {
+				System.out.println("son rectas secantes");
+			}
+			
+		}else if ((a1/a2) == (b1/b2)) {
+			if ((a1/a2) == (b1/b2) && (a1/a2) == (c1/c2) && (b1/b2) == (c1/c2)) {
+				System.out.println("son rectas coincidentes");
+			}else {
+				System.out.println("son rectas paralelas");
+			}
+		}
+		
+		
+		teclado.close();
 	}
 
 }
---
+++ Relacion02/src/Ejercicios/ej07.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+
+public class ej07 {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
