b7b8acc896c81890bc591245b7b53dec66bb319d
Ryder-672 <91911144+Ryder-672@users.noreply.github.com>
2022-12-19 12:30:45 +0100

    07

--- Relacion02/src/Ejercicios/ej07.java
+++ Relacion02/src/Ejercicios/ej07.java
@@ -1,10 +1,23 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class ej07 {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("introduce un año: ");
+		int anio = teclado.nextInt();
+		
+		if ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0))) {
+		
+				System.out.println("es bisiesto");
+		}else {
+				System.out.println("no es bisiesto");
+			}
+		
+		teclado.close();
 	}
 
 }
